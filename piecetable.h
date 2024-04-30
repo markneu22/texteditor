f350b36ecfce89cd063f67c250280b4e1ea45671
markneu22 <markneu22@gmail.com>
2024-04-30 09:29:52 +0200

    Buffer struct has been created

--- piecetable.h
+++ piecetable.h
@@ -1,9 +1,28 @@
 #ifndef piecetable_h
 #define piecetable_h
 //TODO: must use this command: g++ -c main.cpp -o main.o
-// #include <string>
+#include <string>
 
-// static const std::string something = "something";
+struct Buffer
+{
+    const std::string buffer;
+    Buffer() : buffer("") {}
+    Buffer(const std::string& data) : buffer(data){}
+};
 
+
+class PieceTable
+{
+private:
+    Buffer originalBuffer;
+    Buffer addBuffer;
+public:
+    PieceTable(std::string readText);
+    ~PieceTable();
+};
+
+PieceTable::PieceTable(std::string readText) : originalBuffer(readText)
+{
+};
 
 #endif
